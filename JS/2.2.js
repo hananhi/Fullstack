c7160c2c55d623bf8f0aff092cc8ad2e446041f1
hananhi <hananhimoni@gmail.com>
2023-09-20 10:53:05 +0300

    added

---
+++ JS/2.2.js
@@ -0,0 +1,5 @@
+function tellFortune (JopTitle ,location , partnerName ,Numofchildren ){
+
+    console.log('You will be a ' + JopTitle +  ' in ' + location +  ' married to ' + partnerName +  'with ' + Numofchildren + ' children')
+
+}
